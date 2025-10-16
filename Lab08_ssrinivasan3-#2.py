8d14e1e40cc87c9e198dbc4696710b1f775bb3fa
Shrrayashwinz <sshrrayash@gmail.com>
2025-10-16 16:55:54 -0400

    mainhead

--- Lab08_ssrinivasan3-#2.py
+++ Lab08_ssrinivasan3-#2.py
@@ -2,6 +2,23 @@ import Circle as c
 
 import Rectangle as r
 
+def main():
+    while True:
+        print("\n MAIN MENU")
+        print("1. Area of a circle.")
+        print("2. Circumfrence of a circle.")
+        print("3. Area of rectangle.")
+        print("4. Perimeter of rectangle.")
+        print("5. Quit")
+
+        choice = input("Please enture your choice: ")
+
+        if choice == "1":
+            radius = float(input("Please enter the radius: "))
+            print("The Area of the Circle is:", c.area(radius))
+
+main()
+
 
 
         
